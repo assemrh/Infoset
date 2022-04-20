1182e1521a5f850b27d6b4a2039775a403f896ca
Asim Rahal <assem.rh@gmail.com>
2022-04-21 01:25:03 +0300

    up

--- Infoset.Application/Branches/Closest.cs
+++ Infoset.Application/Branches/Closest.cs
@@ -36,6 +36,8 @@ namespace Infoset.Application.Branches
                     if (Distance(currentLocation, temp) <= request.Params.Distance)
                     {
                         branchesNearMe.Add(branch);
+                        if (branchesNearMe.Count >= 5)
+                            break;
                     }
                 }
                 // branches.Where(p => Distance(currentLocation, new Location { Latitude = p.Latitude, Longitude = p.Longitude }) > request.Params.Distance).ToList();
